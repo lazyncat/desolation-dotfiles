c8a18c6f4ec314664915f884b66c207fee02cbad
Noradic <65322459+Pkylik@users.noreply.github.com>
2022-02-05 22:30:40 +1100

    Added polybar gaps
    Gaps for i3 were also adjusted due to such change.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,10 +2,10 @@
 
 killall -q polybar
 
-polybar backup 2>&1 | tee -a /tmp/polybarbackup.log &
+#polybar backup 2>&1 | tee -a /tmp/polybarbackup.log &
 
-#polybar left 2>&1 | tee -a /tmp/polybarleft.log &
-#polybar middle 2>&1 | tee -a /tmp/polybarmiddle.log &
-#polybar right 2>&1 | tee -a /tmp/polybarright.log &
+polybar left 2>&1 | tee -a /tmp/polybarleft.log &
+polybar center 2>&1 | tee -a /tmp/polybarcentre.log &
+polybar right 2>&1 | tee -a /tmp/polybarright.log &
 
 echo "Polybar launched"
